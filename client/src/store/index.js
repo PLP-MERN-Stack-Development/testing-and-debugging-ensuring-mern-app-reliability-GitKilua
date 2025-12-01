f89dae145dff524038a4e781f5c4521479770b7d
GitKilua <rachelgatwiiri@gmail.com>
2025-12-01 10:09:20 +0300

    chore: client README and client fixes (App.jsx, store, vite config)

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -4,5 +4,3 @@ export { default } from './store';
 // Re-export slices for convenience
 export { default as bugsSlice } from './bugSlice';
 
-// If you add more slices (e.g., for auth), export them here too
-// export { default as authSlice } from './authSlice';
